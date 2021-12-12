5ce76c3c9a3d7b6527b755daa09f4db9f8eb9d52
volodymyr <v.ohlopkov100@gmail.com>
2021-12-12 20:58:50 +0200

    v 0.4
    qqq

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ const burger = document.getElementById('btn'),
       modal = document.querySelector(".modal"),
       logo = document.querySelector(".header__logo"),
       body = document.getElementById('body'),
-      closeMenu = document.querySelectorAll('.modal__links');
+      closeMenu = document.querySelectorAll('.modal__links'),
+      head = document.querySelector('.header');
 
 burger.addEventListener('click', (e)=> {
     e.target.classList.toggle('active');
     modal.classList.toggle('active');
     logo.classList.toggle('no--logo');
     body.classList.toggle('no--scroll');
+    head.classList.toggle('no--bg');
 });
 
 closeMenu.forEach(el => el.addEventListener('click', ()=> {
@@ -16,4 +18,17 @@ closeMenu.forEach(el => el.addEventListener('click', ()=> {
     modal.classList.remove('active');
     logo.classList.remove('no--logo');
     body.classList.remove('no--scroll');
+    head.classList.remove('no--bg');
   }));
+
+  let waypoint = new Waypoint({
+    element: document.getElementById('sos'),
+    handler: function(direction) {
+      let header = document.getElementById('header');
+      if(direction === 'down') {
+        header.classList.add('active');
+      } else {
+        header.classList.remove('active');
+      }
+    }
+  });
